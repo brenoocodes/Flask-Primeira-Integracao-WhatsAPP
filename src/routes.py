f8d419f856ecd09dd2c751ffea02c5716168fef3
Breno Carvalho <brenos200304@gmail.com>
2024-07-27 16:04:59 -0300

    modificando

--- src/routes.py
+++ src/routes.py
@@ -1,11 +1,11 @@
 from src.config import app, verify_token
 from flask import jsonify, request
 
-@app.route('/', methods=['GET, POST'])
+@app.route('/', methods=['GET'])
 def home():
     return jsonify({'mensagem': 'API funcionando com sucesso'})
 
-@app.route('/whatsapp', methods=['GET, POST'])
+@app.route('/whatsapp', methods=['POST'])
 def get_whatsapp():
     try:
         received = request.get_json()
